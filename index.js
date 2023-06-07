f655c226fc3ecbc558f5b5fe9ec30706842d0828
Kapil Sharma <kapil.sharma.se@gmail.com>
2023-06-07 21:23:39 +0200

    chore: add manual test logs

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const {deterministicPartitionKey} = require("./dpk");
+const { deterministicPartitionKey } = require("./dpk");
 
-console.log(deterministicPartitionKey());
+const longString = "a".repeat(300);
+
+console.log("1. No Input:\n  ", deterministicPartitionKey());
+console.log("\n2. Falsy Event:\n  ", deterministicPartitionKey(null));
+console.log("\n3. With partitionKey:\n  ", deterministicPartitionKey({ partitionKey: "abc123" }));
+console.log("\n4. Event without partitionKey:\n  ", deterministicPartitionKey({ foo: "bar" }));
+console.log("\n5. Event without partitionKey:\n  ", deterministicPartitionKey({ foo: "bar" }));
+console.log("\n6. Event with partitionKey longer than MAX_PARTITION_KEY_LENGTH:\n  ", deterministicPartitionKey({ partitionKey: longString }));
